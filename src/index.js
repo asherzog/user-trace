db7de3849e1469604ddc52a71d971aeaba5df6b1
Andy Herzog <asherzog19@gmail.com>
2019-07-14 18:26:50 -0600

    initial happy path addAction

---
+++ src/index.js
@@ -0,0 +1,37 @@
+class UserTrace {
+    constructor() {
+        this.actions = {}
+    }
+
+    addAction(serializedEvent) {
+        try {
+            // TODO: validte input
+            const event = JSON.parse(serializedEvent)
+            const { action, time } = event
+            if (this.actions[action]) {
+                this.actions = {
+                    ...this.actions,
+                    [action]: {
+                        ...this.actions[action], 
+                        count: this.actions[action].count + 1,
+                        time: (this.actions[action].time * this.actions[action].count + time) / (this.actions[action].count + 1)
+                    }
+                }
+            } else {
+                this.actions = {
+                    ...this.actions,
+                    [action]: {
+                        action,
+                        time,
+                        count: 1
+                    } 
+                }
+            }
+        } catch (err) {
+            // TODO: Error handling
+            throw new Error('invalid input')
+        }
+    }
+}
+
+module.exports = { UserTrace } 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,73 @@
 const expect = require('chai').expect
+const { UserTrace } = require('../src')
 
 describe('UserTrace Module', () => {
+    describe('Instance', () => {
+        it('should instanciate new UserTrace with empty action stats', () => {
+            const test = new UserTrace()
+            const expected = { actions: {} }
+            expect(test).to.deep.equal(expected)
+        })
+        it('should instanciate multiple new UserTraces with empty action stats', () => {
+            const test = new UserTrace()
+            const test2 = new UserTrace()
+            const expected = { actions: {} }
+            expect(test).to.deep.equal(expected)
+            expect(test2).to.deep.equal(expected)
+        })
+    })
     describe('addAction method', () => {
-        it('should do something', () => {
-            expect('test').to.equal('test')
+        it('should add a new action', () => {
+            const test = new UserTrace()
+            test.addAction(JSON.stringify({ action: 'jump', time: 100 }))
+            const expected = {
+                actions: {
+                    jump: { action: 'jump', time: 100, count: 1 }
+                }
+            }
+            expect(test).to.deep.equal(expected)
+        })
+        it('should add two separate actions', () => {
+            const test = new UserTrace()
+            test.addAction(JSON.stringify({ action: 'jump', time: 100 }))
+            test.addAction(JSON.stringify({ action: 'run', time: 100 }))
+            const expected = {
+                actions: {
+                    jump: { action: 'jump', time: 100, count: 1 },
+                    run: { action: 'run', time: 100, count: 1 }
+                }
+            }
+            expect(test).to.deep.equal(expected)
+        })
+        it('should track like actions and average their time', () => {
+            const test = new UserTrace()
+            test.addAction(JSON.stringify({ action: 'jump', time: 100 }))
+            test.addAction(JSON.stringify({ action: 'jump', time: 200 }))
+            const expected = {
+                actions: {
+                    jump: { action: 'jump', time: 150, count: 2 }
+                }
+            }
+            expect(test).to.deep.equal(expected)
+        })
+        it('should differentiate instances', () => {
+            const test = new UserTrace()
+            const test2 = new UserTrace()
+            test.addAction(JSON.stringify({ action: 'jump', time: 100 }))
+            test.addAction(JSON.stringify({ action: 'jump', time: 200 }))
+            test2.addAction(JSON.stringify({ action: 'jump', time: 200 }))
+            const expectedTest1 = {
+                actions: {
+                    jump: { action: 'jump', time: 150, count: 2 }
+                }
+            }
+            const expectedTest2 = {
+                actions: {
+                    jump: { action: 'jump', time: 200, count: 1 }
+                }
+            }
+            expect(test).to.deep.equal(expectedTest1)
+            expect(test2).to.deep.equal(expectedTest2)
         })
     })
 
